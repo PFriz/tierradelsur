702ebaeb7080aacaead11896b9f08b731aff10eb
xpattox <patto2292@gmail.com>
2017-08-14 00:03:55 -0300

    actualizacion de carrusel/sube fotos
    y se establece de forma secundaria donde guardar las rutasde las imagenes subidas.

--- proyasp/ProyectoPapurri/ProyectoPapurri/Private/Carrusel-nuevafoto.aspx.cs
+++ proyasp/ProyectoPapurri/ProyectoPapurri/Private/Carrusel-nuevafoto.aspx.cs
@@ -13,22 +13,24 @@ namespace ProyectoPapurri.Private
         protected void Page_Load(object sender, EventArgs e)
         {
             NImagen.Click += new EventHandler(this.NuevaImagen);
+            plus.Text = "";
         }
         private void NuevaImagen(object sender, EventArgs e)
         {
             //guardar imagenes
-            if ((archivo.PostedFile != null) && (archivo.PostedFile.ContentLength > 0))
+            if ((archivo1.PostedFile != null) && (archivo1.PostedFile.ContentLength > 0))
             {
-                string fn = System.IO.Path.GetFileName(archivo.PostedFile.FileName);
-                string SaveLocation = Server.MapPath("carrusel") + "\\" + fn;
+                string fn = System.IO.Path.GetFileName(archivo1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("..\\assets\\img\\Carrusel") + "\\" + fn;
                 try
                 {
-                    archivo.PostedFile.SaveAs(SaveLocation);
-                    Response.Write("The file has been uploaded.");
+                    archivo1.PostedFile.SaveAs(SaveLocation);
+                    //DB guardar en un tabla la ubicacion de las imagenees para cargarlas mas adelante
+                    plus.Text += "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>¡Excelente! Su imagen a sido subida exitosamente</div>";
                 }
                 catch (Exception ex)
                 {
-                    Response.Write("Error: " + ex.Message);
+                    //Response.Write("Error: " + ex.Message);
                     //Note: Exception.Message returns a detailed message that describes the current exception. 
                     //For security reasons, we do not recommend that you return Exception.Message to end users in 
                     //production environments. It would be better to put a generic error message. 
@@ -36,8 +38,66 @@ namespace ProyectoPapurri.Private
             }
             else
             {
-                Response.Write("Please select a file to upload.");
+                plus.Text = "Al menos seleccione un archivo";
             }
+
+            if ((archivo2.PostedFile != null) && (archivo2.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(archivo2.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("..\\assets\\img\\Carrusel") + "\\" + fn;
+                try
+                {
+                    archivo2.PostedFile.SaveAs(SaveLocation);
+                    //DB guardar en un tabla la ubicacion de las imagenees para cargarlas mas adelante
+                    plus.Text += "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>¡Excelente! Su imagen a sido subida exitosamente</div>";
+                }
+                catch (Exception ex)
+                {
+                    //Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns a detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                    //production environments. It would be better to put a generic error message. 
+                }
+            }
+            if ((archivo3.PostedFile != null) && (archivo3.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(archivo3.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("..\\assets\\img\\Carrusel") + "\\" + fn;
+                try
+                {
+                    archivo3.PostedFile.SaveAs(SaveLocation);
+                    //DB guardar en un tabla la ubicacion de las imagenees para cargarlas mas adelante
+                    plus.Text += "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>¡Excelente! Su imagen a sido subida exitosamente</div>";
+
+                }
+                catch (Exception ex)
+                {
+                    //Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns a detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                    //production environments. It would be better to put a generic error message. 
+                }
+            }
+            if ((archivo4.PostedFile != null) && (archivo4.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(archivo4.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("..\\assets\\img\\Carrusel") + "\\" + fn;
+                try
+                {
+                    archivo4.PostedFile.SaveAs(SaveLocation);
+                    //DB guardar en un tabla la ubicacion de las imagenees para cargarlas mas adelante
+                    plus.Text += "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>¡Excelente! Su imagen a sido subida exitosamente</div>";
+
+                }
+                catch (Exception ex)
+                {
+                    //Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns a detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                    //production environments. It would be better to put a generic error message. 
+                }
+            }
+
 
         }
     }
--- proyasp/ProyectoPapurri/ProyectoPapurri/Private/Carrusel-nuevafoto.aspx.designer.cs
+++ proyasp/ProyectoPapurri/ProyectoPapurri/Private/Carrusel-nuevafoto.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace ProyectoPapurri.Private {
     public partial class Carrusel_nuevafoto {
         
         /// <summary>
-        /// Control archivo.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputFile archivo;
-        
-        /// <summary>
         /// Control plus.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,42 @@ namespace ProyectoPapurri.Private {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal plus;
+        
+        /// <summary>
+        /// Control archivo1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile archivo1;
+        
+        /// <summary>
+        /// Control archivo2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile archivo2;
+        
+        /// <summary>
+        /// Control archivo3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile archivo3;
+        
+        /// <summary>
+        /// Control archivo4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile archivo4;
         
         /// <summary>
         /// Control NImagen.
